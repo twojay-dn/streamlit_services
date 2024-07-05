456e1bfbd948bffae44e15729fe1e54210d7d642
twojay-dn <156738908+twojay-dn@users.noreply.github.com>
2024-07-05 19:23:10 +0900

    update

--- Views/Chat.py
+++ Views/Chat.py
@@ -1,6 +1,12 @@
 import streamlit as st
 from States import State
 from Classes import Agent
+from utils import read_file
+
+def compose_prompt(prompt : str, params : dict) -> str:
+    if params is None:
+        return prompt
+    return prompt.format(**params)
 
 def render():
     st.title("Chat")
@@ -9,9 +15,17 @@ def render():
     if curriculum is None:
         st.error("Curriculum is not set")
         return
-    teacher_persona
-    teacher = Agent("assistant", "학생의 질문에 답변을 제공하는 역할을 합니다.")
-    student = Agent("user", "선생님의 질문에 답변을 제공하는 역할을 합니다.")
+    teacher_persona_params = {
+        "grade" : State.get("grade"),
+        "subject" : State.get("subject"),
+        "unit" : State.get("unit"),
+        "title" : State.get("title"),
+        "content" : curriculum
+    }
+    teacher_persona = compose_prompt(read_file("prompts/persona_teacher.md"), teacher_persona_params)
+    student_persona = compose_prompt(read_file("prompts/persona_student.md"), None)
+    teacher = Agent("assistant", teacher_persona)
+    student = Agent("user", student_persona)
 
     if st.button("Start"):
         former_message = ""
