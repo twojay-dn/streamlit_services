0db615896ec65cfb96f73bebe9e493279702d3a7
twojay-dn <156738908+twojay-dn@users.noreply.github.com>
2024-07-19 19:43:17 +0900

    page 헤더 수정

--- src/Views/Pages/Quiz_v1.py
+++ src/Views/Pages/Quiz_v1.py
@@ -2,7 +2,7 @@ from src.Views.Components import render_page, BaseTabs, BaseColumns
 from src.Views.Pages.Sections.Quiz_sections2 import generation_column, chat_column
 import streamlit as st
 
-@render_page(name="Quiz")
+@render_page(name="Quiz_v1")
 def page():
     tabs = BaseColumns([
         generation_column,
--- src/Views/Pages/Quiz_v2.py
+++ src/Views/Pages/Quiz_v2.py
@@ -2,7 +2,7 @@ from src.Views.Components import render_page, BaseTabs, BaseColumns
 from src.Views.Pages.Sections.Quiz_sections3 import generation_column, chat_column
 import streamlit as st
 
-@render_page(name="Quiz")
+@render_page(name="Quiz_v2")
 def page():
     tabs = BaseColumns([
         generation_column,
