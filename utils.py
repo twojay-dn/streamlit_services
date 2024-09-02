a3d76b765fe985967cb8148184f1168c1841d383
twojay-dn <156738908+twojay-dn@users.noreply.github.com>
2024-09-02 10:13:36 +0900

    branch update

--- utils.py
+++
@@ -1,7 +0,0 @@
-import os
-
-def read_file(file_path: str) -> str:
-    if not os.path.exists(file_path):
-        raise FileNotFoundError(f"File {file_path} not found")
-    with open(file_path, "r") as file:
-        return file.read()
